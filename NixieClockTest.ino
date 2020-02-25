1fbf293b5c5f4b79b5962abf94a8ab59967afd2b
aviateur17 <61305397+aviateur17@users.noreply.github.com>
2020-02-25 06:20:24 -0600

    Add files via upload

---
+++ NixieClockTest.ino
@@ -0,0 +1,128 @@
+#define A1 3
+#define B1 4
+#define C1 5
+#define D1 6
+#define A2 7
+#define B2 8
+#define C2 9
+#define D2 10
+#define A3 11
+#define B3 12
+#define C3 13
+#define D3 14
+#define A4 15
+#define B4 16
+#define C4 2
+#define D4 17
+
+char A[4] = {A1, A2, A3, A4};
+char B[4] = {B1, B2, B3, B4};
+char C[4] = {C1, C2, C3, C4};
+char D[4] = {D1, D2, D3, D4};
+
+void setup() {
+  pinMode(A1, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(C1, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B2, OUTPUT);
+  pinMode(C2, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(B3, OUTPUT);
+  pinMode(C3, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(B4, OUTPUT);
+  pinMode(C4, OUTPUT);
+  pinMode(D4, OUTPUT);
+  
+  for (char i = 0; i < 4; i++) {
+    digitalWrite(A[i], HIGH);
+    digitalWrite(B[i], HIGH);
+    digitalWrite(C[i], HIGH);
+    digitalWrite(D[i], HIGH);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+  for (int m = 0; m < 4; m++){
+    writenumber(m, i);
+  }
+    delay(500);
+  }
+}
+
+void writenumber(int a, int b) {
+  switch (b) {
+    case 0:
+      digitalWrite(A[a], LOW);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], LOW);
+      break;
+    case 9:
+      digitalWrite(A[a], HIGH);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], LOW);
+      break;
+    case 8:
+      digitalWrite(A[a], LOW);
+      digitalWrite(B[a], HIGH);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], LOW);
+      break;
+    case 7:
+      digitalWrite(A[a], HIGH);
+      digitalWrite(B[a], HIGH);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], LOW);
+      break;
+    case 6:
+      digitalWrite(A[a], LOW);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], HIGH);
+      digitalWrite(D[a], LOW);
+      break;
+    case 5:
+      digitalWrite(A[a], HIGH);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], HIGH);
+      digitalWrite(D[a], LOW);
+      break;
+    case 4:
+      digitalWrite(A[a], LOW);
+      digitalWrite(B[a], HIGH);
+      digitalWrite(C[a], HIGH);
+      digitalWrite(D[a], LOW);
+      break;
+    case 3:
+      digitalWrite(A[a], HIGH);
+      digitalWrite(B[a], HIGH);
+      digitalWrite(C[a], HIGH);
+      digitalWrite(D[a], LOW);
+      break;
+    case 2:
+      digitalWrite(A[a], LOW);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], HIGH);
+      break;
+    case 1:
+      digitalWrite(A[a], HIGH);
+      digitalWrite(B[a], LOW);
+      digitalWrite(C[a], LOW);
+      digitalWrite(D[a], HIGH);
+      break;
+  }
+}
+
+void off(int a) {
+  digitalWrite(A[a], HIGH);
+  digitalWrite(B[a], HIGH);
+  digitalWrite(C[a], HIGH);
+  digitalWrite(D[a], HIGH);
+}
